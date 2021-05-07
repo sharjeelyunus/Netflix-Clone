62eb14447633ac1706d3a3ad9f883fc643dae640
sharjeelyunus <sharjeel924@gmail.com>
2021-05-08 03:30:36 +0500

    📦 NEW: Built Banner

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Clone</h1>
+      <Banner />
       <Row
         title="NETFLIX ORIGNAL"
         fetchUrl={requests.fetchNetflixOriginals}
